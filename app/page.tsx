ec754765e85d7c947b7b6740e6c9fb4f67f209ca
Nati <nathanmes124@gmail.com>
2025-02-24 18:53:03 +0300

    hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-const page = () => {
-  return <div>Test commit</div>;
+import Hero from "@/components/Hero";
+
+const Home = () => {
+  return (
+    <main className="reltive bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
+  );
 };
 
-export default page;
+export default Home;
